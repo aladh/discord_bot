ea07dc60d1e7a2b6beb03316a3070561ece2b864
Alishan Ladhani <alishan.ladhani@gmail.com>
2020-05-09 16:55:11 -0400

    Create end to end test for spotify handler

---
+++ main_test.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"log"
+	"os"
+	"testing"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/zmb3/spotify"
+	"golang.org/x/oauth2"
+
+	"github.com/ali-l/discord_bot_go/config"
+)
+
+func TestSpotifyAddToPlaylist(t *testing.T) {
+	_, enabled := os.LookupEnv("ENABLE_E2E_TESTS")
+	if !enabled {
+		t.Skip("End to end tests are not enabled")
+	}
+
+	cfg, err := config.FromEnv()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	sendAndDeleteSpotifyLink(t, cfg.DiscordToken, os.Getenv("DISCORD_CHANNEL_ID"), os.Getenv("SPOTIFY_TRACK_LINK"))
+
+	spotifyClient := createSpotifyClient(cfg)
+
+	track := getLastTrack(t, spotifyClient, cfg.SpotifyPlaylistID)
+	expectedTrackName := os.Getenv("SPOTIFY_TRACK_NAME")
+
+	if track.Name != expectedTrackName {
+		t.Fatalf("track = %s, want %s", track.Name, expectedTrackName)
+	}
+
+	_, err = spotifyClient.RemoveTracksFromPlaylist(spotify.ID(cfg.SpotifyPlaylistID), track.ID)
+	if err != nil {
+		t.Fatalf("error removing track %s from playlist: %s", track.Name, err)
+	}
+}
+
+func getLastTrack(t *testing.T, client spotify.Client, playlistId string) *spotify.FullTrack {
+	playlist, err := client.GetPlaylist(spotify.ID(playlistId))
+	if err != nil {
+		t.Fatalf("error getting playlist: %s", err)
+	}
+
+	lastTrack := playlist.Tracks.Total - 1
+	limit := int(1)
+
+	tracks, err := client.GetPlaylistTracksOpt(spotify.ID(playlistId), &spotify.Options{Limit: &limit, Offset: &lastTrack}, "items(track(id,name))")
+	if err != nil {
+		t.Fatalf("error getting playlist tracks: %s", err)
+	}
+
+	return &tracks.Tracks[0].Track
+}
+
+func createSpotifyClient(cfg *config.Config) spotify.Client {
+	auth := spotify.NewAuthenticator("", spotify.ScopePlaylistModifyPublic)
+	auth.SetAuthInfo(cfg.SpotifyClientID, cfg.SpotifyClientSecret)
+	client := auth.NewClient(&oauth2.Token{TokenType: "Bearer", RefreshToken: cfg.SpotifyRefreshToken})
+
+	return client
+}
+
+func sendAndDeleteSpotifyLink(t *testing.T, token string, channelId string, trackLink string) {
+	session := createDiscordSession(t, token)
+	defer closeSession(session)
+
+	msg, err := session.ChannelMessageSend(channelId, trackLink)
+	if err != nil {
+		t.Fatalf("error sending message: %s", err)
+	}
+
+	err = session.ChannelMessageDelete(channelId, msg.ID)
+	if err != nil {
+		t.Fatalf("error deleting message: %s", err)
+	}
+}
+
+func createDiscordSession(t *testing.T, discordToken string) *discordgo.Session {
+	session, err := discordgo.New("Bot " + discordToken)
+	if err != nil {
+		t.Fatalf("error initializng session: %s", err)
+	}
+
+	err = session.Open()
+	if err != nil {
+		t.Fatalf("error opening connection: %s", err)
+	}
+
+	return session
+}
+
+func closeSession(session *discordgo.Session) {
+	err := session.Close()
+	if err != nil {
+		log.Fatalf("error closing session: %s\n", err)
+	}
+}
